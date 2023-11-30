73db17e12a633b31839e0e226ecbf67281a767fe
KunalBhika <kunalbhika2@gmail.com>
2023-11-30 11:06:49 +0530

    made a login endpoint and fetched userDetails from DB using authtoken provided to user while signup

---
+++ backend/middleWare/fetchuser.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = 'KunalBhika1#';
+
+const fetchUser = async (req, res, next) => {
+    const token = req.header('auth-token');
+    if(!token) {
+        return res.status(401).json('Access Denied! Please try to login using correct token');
+    }
+
+    try {
+        const data = jwt.verify(token, JWT_SECRET);
+        req.userId = data.userId;
+        next();
+    } catch (error) {
+        res.send(error);
+    }
+}
+
+module.exports = fetchUser;
